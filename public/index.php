03fe7098bbe3fcb5712afa72b8b4fe6834e6932b
Rosi <rosy28_84@hotmail.com>
2016-06-23 16:03:36 -0400

    NuevoF

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__.'/../RepoFinal/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/../RepoFinal/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
